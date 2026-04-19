04ea80622fd2eff3c337e3e59627920f4ff6396c
MIRYAMG03 <93146036+MIRYAMG03@users.noreply.github.com>
2026-04-19 16:51:49 -0600

    Update connection.js

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,18 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', // tu contraseña si tienes
-    database: 'inventario_celulares'
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error de conexión:', err);
-    } else {
-        console.log('Conectado a MySQL');
-    }
+  if (err) {
+    console.error("Error de conexión:", err);
+  } else {
+    console.log("Conectado a MySQL (Railway)");
+  }
 });
 
 module.exports = connection;
